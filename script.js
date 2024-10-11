ab21afddce6c5d16620f453e0ad1a5a91cab650d
JulioPe13 <julioeliasdev@gmail.com>
2024-10-11 04:25:41 -0500

    Solution Bug

--- script.js
+++ script.js
@@ -61,41 +61,12 @@ if (horaactual >= 19 || horaactual < 6){
 
 // DETERMINAR pm / am
 
-/*
-if(horaactual > 12 && horaactual < Number(23)){
+
+if(horaactual >=12 && horaactual < 23){
     ampm.textContent ='pm'
 }else{
    ampm.textContent = 'am'    
 }
 }
-*/
-
-
 
 imgactualizar();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
